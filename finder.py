da7820be35e4c42a8143d1508a2d616034ba8dac
sbird <sbird@disroot.org>
2024-12-27 16:28:05 +0800

    added calculation of time of existence

--- finder.py
+++ finder.py
@@ -1,5 +1,5 @@
 import time
-import datetime
+from datetime import date
 from colorama import Fore, Back
 
 MONTH_TO_STR = {
@@ -18,8 +18,9 @@ MONTH_TO_STR = {
 }
 
 while True:
+    #* ask for year *#
     year = 0
-    while year <= 0 or year > datetime.date.today().year:
+    while year <= 0 or year > date.today().year:
         try:
             year = int(input(Fore.BLUE + 'please enter your year of birth: '))
             
@@ -27,13 +28,14 @@ while True:
                 print(Fore.RED + 'there\'s no such thing as a year 0')
             if year < 0:
                 print(Fore.RED + 'you can\'t enter a negative year')
-            if year > datetime.date.today().year:
+            if year > date.today().year:
                 print(Fore.RED + 'you can\'t be born in the future')
         except:
             print(Fore.RED + 'please enter a number for year')
     print('year of birth set!')
     print('\033c', end='') # clear terminal
 
+    #* ask for month *#
     month = 0
     while month <= 0 or month > 12: # month must be between 1 and 12
         try:
@@ -50,9 +52,9 @@ while True:
     print('month of birth set!')
     print('\033c', end='') # clear terminal
 
-    # sets maximum number of days
+    #* ask for day *#
     MAX_DAYS = 0
-    match month:
+    match month: # finds how many days in month
         case 1 | 3 | 5 | 7 | 8 | 10 | 12: # months with 31 days
             MAX_DAYS = 31
         case 4 | 6 | 9 | 11: # months with 30 days
@@ -78,3 +80,56 @@ while True:
             print(Fore.RED + 'please enter a number for day')
     print('day of birth set!')
     print('\033c', end='') # clear terminal
+
+    #* lists age of thing *#
+    current_date = date.today()
+
+    TimeExisting = [0, 0, 0] # declares array containing no. of years, months, and days
+
+    # checks if birthday has been passed or not
+    if current_date.month >= month and current_date.day >= day:
+        # difference between current year and year of birth 
+        TimeExisting[0] = current_date.year - year 
+    else:
+        # subtracts one if birthday has not passed this year
+        TimeExisting[0] = (current_date.year - year)-1 
+    
+    # checks if day of birth has been passed or not
+    if current_date.day >= day:
+        # difference between current month and month of birth
+        TimeExisting[1] = current_date.month - month 
+    else:
+        # subtracts one if month has not passed the day
+        TimeExisting[1] = (current_date.month - month)-1
+
+    # adds twelve to months existing if negative
+    if TimeExisting[1] < 0:
+        TimeExisting[1] = 12 + TimeExisting[1]
+    
+    MAX_DAYS_MONTH_BEFORE_CURRENT = 0
+    match current_date.month - 1: # finds max days of month before current month
+        case 1 | 3 | 5 | 7 | 8 | 10 | 12: # months with 31 days
+            MAX_DAYS_MONTH_BEFORE_CURRENT = 31
+        case 4 | 6 | 9 | 11: # months with 30 days
+            MAX_DAYS_MONTH_BEFORE_CURRENT = 30
+        case 2: # february
+            if year % 4 == 0: # leap years
+                MAX_DAYS_MONTH_BEFORE_CURRENT = 29
+            else:
+                MAX_DAYS_MONTH_BEFORE_CURRENT = 28
+
+    # finds difference between current day and day of birth
+    TimeExisting[2] = current_date.day - day
+    # adds twelve to days existing if negative
+    if TimeExisting[2] < 0: 
+        TimeExisting[2] = MAX_DAYS_MONTH_BEFORE_CURRENT + TimeExisting[2]
+
+    TOTAL_MONTHS = (TimeExisting[0] * 12) + TimeExisting[1] # finds how many months thing has existed for
+
+    print(f'you have lived for {TimeExisting[0]} years {TimeExisting[1]} months and {TimeExisting[2]} days')
+    print(f'you have lived for {TOTAL_MONTHS} months')
+    input()
+
+    print('\033c', end='') # clear terminal
+    
+    
