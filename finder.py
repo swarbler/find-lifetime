b90d7920fd0b64431216da137b3ed4a92124650b
sbird <sbird@disroot.org>
2024-12-27 16:01:40 +0800

    added days, removed title text

--- finder.py
+++ finder.py
@@ -1,12 +1,23 @@
 import time
 import datetime
 from colorama import Fore, Back
-from art import *
+
+MONTH_TO_STR = {
+    1: 'january',
+    2: 'february',
+    3: 'march',
+    4: 'april',
+    5: 'may',
+    6: 'june',
+    7: 'july',
+    8: 'august',
+    9: 'september',
+    10: 'october',
+    11: 'november',
+    12: 'december',
+}
 
 while True:
-    print(Fore.MAGENTA, end='')
-    tprint('lifetime finder')
-
     year = 0
     while year <= 0 or year > datetime.date.today().year:
         try:
@@ -22,9 +33,6 @@ while True:
             print(Fore.RED + 'please enter a number for year')
     print('year of birth set!')
     print('\033c', end='') # clear terminal
-
-    print(Fore.MAGENTA, end='')
-    tprint('lifetime finder')
 
     month = 0
     while month <= 0 or month > 12: # month must be between 1 and 12
@@ -43,6 +51,7 @@ while True:
     print('\033c', end='') # clear terminal
 
     # sets maximum number of days
+    MAX_DAYS = 0
     match month:
         case 1 | 3 | 5 | 7 | 8 | 10 | 12: # months with 31 days
             MAX_DAYS = 31
@@ -53,3 +62,19 @@ while True:
                 MAX_DAYS = 29
             else:
                 MAX_DAYS = 28
+
+    day = 0
+    while day <= 0 or month > MAX_DAYS: # month must be between 1 and MAX_DAYS
+        try:
+            day = int(input(Fore.GREEN + 'please enter your day of birth: '))
+            
+            if day == 0:
+                print(Fore.RED + 'there\'s no such thing as a day 0')
+            if day < 0:
+                print(Fore.RED + 'there\'s no such thing as a negative day')
+            if day >= MAX_DAYS:
+                print(Fore.RED + f'there\'s only {MAX_DAYS} days in {MONTH_TO_STR[month]}')
+        except:
+            print(Fore.RED + 'please enter a number for day')
+    print('day of birth set!')
+    print('\033c', end='') # clear terminal
